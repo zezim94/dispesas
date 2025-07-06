a0d16c64c07a8442da8db02962ffd37511581556
zezim94 <andelsonascimento@gmail.com>
2025-07-06 08:56:12 -0300

    Update login.php

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Relatório Mensal</title>
+    <title>Login - Controle Finenceiro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
