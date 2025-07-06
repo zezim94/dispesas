3d9978c22eba9408d997b43001d13a2ccd788ea3
zezim94 <andelsonascimento@gmail.com>
2025-07-06 07:51:34 -0300

    Update pedidos.php

--- pedidos.php
+++ pedidos.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id']) || $_SESSION['NIVEL'] !== 'adm') {
+if (!isset($_SESSION['user_id']) || $_SESSION['nivel'] !== 'adm') {
     header('Location: login.php');  // Redireciona para a página de login ou página normal
     exit;
 }
